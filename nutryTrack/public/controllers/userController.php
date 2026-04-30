8fc623ce87ef2279588926e75536175b9c676d79
Arnau Barón (PC) <arnauferrer67@gmail.com>
2026-04-30 17:04:02 +0200

    Terminada la funcionalidad para inciar sesión, registrarse y cerrar la sesión, pendiente testeo

--- nutryTrack/public/controllers/userController.php
+++ nutryTrack/public/controllers/userController.php
@@ -17,7 +17,7 @@ class UserController {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $error = null;
             $email = $_POST['email'];
-            $userName = $_POST['username'];
+            $username = $_POST['username'];
             $passwordPlana = $_POST['password'];
 
             if ($this -> auth -> checkMail($email)) {
@@ -26,7 +26,7 @@ class UserController {
 
             }
 
-            if ($this -> auth -> checkUser($userName)) {
+            if ($this -> auth -> checkUser($username)) {
 
                 $error = "El usuario ya existe";
 
@@ -36,7 +36,7 @@ class UserController {
 
                 $passwordHash = $this -> auth -> hashPassword($passwordPlana);
 
-                $nuevoUsuario = new User($email, $userName, $passwordHash);
+                $nuevoUsuario = new User($email, $username, $passwordHash);
 
                 $this -> gestor -> registrarUsuario($nuevoUsuario);
 
@@ -46,6 +46,76 @@ class UserController {
         }
 
         include "views/alta.php";
+    }
+
+    public function login() {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $recordar = isset($_POST['recordarme']);
+
+            $user = $this -> gestor -> buscarUsuarioPorEmail($email);
+
+            if ($user && $this -> auth -> verifyPassword($password, $user -> getPassword())) {
+
+                $_SESSION['id'] = $user -> getId();
+                $_SESSION['email'] = $user -> getEmail();
+                $_SESSION['username'] = $user -> getUserName();
+
+                if ($recordar) {
+
+                    $token = base64_encode($user -> getEmail());
+
+                    setcookie(
+
+                        "usuario_login",
+                        $token,
+                        [
+
+                            'expires' => time() + (86400 * 3),
+                            'path' => '/',
+                            'httponly' => true,
+                            'samesite' => 'Strict'
+
+                        ]
+
+                    );
+
+                }
+                
+                header("Location: index.php");
+                exit;
+
+            } else {
+
+                $error = "Credenciales Incorrectas.";
+
+            }
+
+        }
+
+        include "views/login.php";
+
+    }
+
+    public function logout() {
+
+        $_SESSION = [];
+
+        session_destroy();
+
+        if (isset($_COOKIE['usuario_login'])) {
+
+            setcookie('usuario_login', '', time() - 3600000, '/');
+
+        }
+
+        header("Location: index.php?accion=login");
+        exit;
+
     }
 
 }
