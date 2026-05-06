e0af0291611340c34d836d24c53e6a00615321eb
Arnau Barón (PC) <arnauferrer67@gmail.com>
2026-05-06 17:24:27 +0200

    cambios

--- nutryTrack/public/controllers/userController.php
+++ nutryTrack/public/controllers/userController.php
@@ -53,7 +53,7 @@ class UserController {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $email = $_POST['email'];
-             
+            
             $password = $_POST['password'];
             $recordar = isset($_POST['recordarme']);
 
