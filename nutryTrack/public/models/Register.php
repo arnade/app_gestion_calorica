799028a6fd9d6f1603a36d5bcd8e383fabc06b77
Arnau Barón (PC) <arnauferrer67@gmail.com>
2026-05-12 12:42:12 +0200

    Añdadidos finales

--- nutryTrack/public/models/Register.php
+++ nutryTrack/public/models/Register.php
@@ -1,101 +1,122 @@
 <?php
 
-class Register {
+class Register
+{
 
-    
+
     protected $id;
     protected $user;
     protected $recipe;
     protected $date;
-    protected $grams; 
+    protected $grams;
 
-    public function __construct($recipe, $date, $grams, $user = null,  $id=0){
-        
-        $this->recipe=$recipe;
-        $this->date=$date;
-        $this->grams=$grams;
-        $this->user=$user;
-        $this->id=$id;
+    public function __construct($recipe, $date, $grams, $user = null, $id = 0)
+    {
+        $this->recipe = $recipe;
+        $this->date = $date;
+        $this->setGrams($grams);
+        $this->user = $user;
+        $this->id = $id;
     }
 
-    public function calculateProteins(){
-        $totalProteins= ($this->recipe->getProteins() * $this->grams)/100 ;
-        return $totalProteins; 
+    public function calculateProteins()
+    {
+        $totalProteins = ($this->recipe->getProteins() * $this->grams) / 100;
+        return $totalProteins;
     }
 
-    public function calculateCarbs(){
-        $totalCarbs= ($this->recipe->getCarbs() * $this->grams)/100 ;
-        return $totalCarbs; 
+    public function calculateCarbs()
+    {
+        $totalCarbs = ($this->recipe->getCarbs() * $this->grams) / 100;
+        return $totalCarbs;
     }
 
-    public function calculateFats(){
-        $totalFats= ($this->recipe->getFats() * $this->grams)/100 ;
-        return $totalFats; 
+    public function calculateFats()
+    {
+        $totalFats = ($this->recipe->getFats() * $this->grams) / 100;
+        return $totalFats;
     }
 
-        public function calculateKcals(){
-        $totalKcals= ($this->recipe->getKcals() * $this->grams)/100 ;
-        return $totalKcals; 
+    public function calculateKcals()
+    {
+        $totalKcals = ($this->recipe->getKcals() * $this->grams) / 100;
+        return $totalKcals;
     }
 
-    public function getUserId(){
+    public function getUserId()
+    {
         $userId = $this->user->getId();
         return $userId;
     }
 
-    public function getRecipeId(){
+    public function getRecipeId()
+    {
         $recipeId = $this->recipe->getId();
         return $recipeId;
     }
 
-    public function getRecipeName(){
-        $recipeName= $this->recipe->getName();
+    public function getRecipeName()
+    {
+        $recipeName = $this->recipe->getName();
         return $recipeName;
     }
 
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function setId($id){
+    public function setId($id)
+    {
         $this->id = $id;
         return $this;
     }
 
-    public function getRecipe(){
+    public function getRecipe()
+    {
         return $this->recipe;
     }
 
-    public function setRecipe($recipe){
+    public function setRecipe($recipe)
+    {
         $this->recipe = $recipe;
         return $this;
     }
 
-    public function getDate(){
+    public function getDate()
+    {
         return $this->date;
     }
 
-    public function setDate($date){
+    public function setDate($date)
+    {
         $this->date = $date;
         return $this;
     }
 
-    public function getGrams(){
+    public function getGrams()
+    {
         return $this->grams;
     }
 
-    public function setGrams($grams){
-        $this->grams = $grams;
+    public function setGrams($grams)
+    {
+        if ($grams < 0) {
+            $this->grams = 1;
+        } else {
+            $this->grams = $grams;
+        }
         return $this;
     }
 
-    public function getUser(){
+    public function getUser()
+    {
         return $this->user;
     }
 
-    public function setUser($user){
+    public function setUser($user)
+    {
         $this->user = $user;
         return $this;
     }
-
 }
